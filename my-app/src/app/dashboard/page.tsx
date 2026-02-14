2107aff189d90e7c1dfad5d98a4c8719b1826d6b
chandrasekhar <chandrasekharsathavalli99@gmail.com>
2026-02-15 00:45:35 +0530

    Add my-app project as normal folder

---
+++ my-app/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+const page = () => {
+  console.log("Dashboard page rendered");
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Dashboard Page</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import {supabase} from "@/lib/supabaseClient";
+
+const page = () => {
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `http://localhost:3000/dashboard`,
+      },
+    });
+    if (error) console.error("Error during login:", error.message);
+  }
+ 
+    return (
+      <div className="flex items-center justify-center min-h-screen px-4 
+        bg-gradient-to-br from-zinc-900 via-purple-900 to-zinc-950">
+
+        <div className="w-full max-w-md sm:max-w-lg
+          flex flex-col items-center justify-center
+          min-h-[360px] sm:min-h-[420px]
+          p-6 sm:p-8
+          bg-white/10 backdrop-blur-xl
+          border border-white/20
+          rounded-2xl shadow-2xl
+          text-white">
+
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center">
+            Smart Bookmark App
+          </h1>
+
+          <button
+            className="w-full sm:w-auto
+              bg-blue-500 hover:bg-blue-700
+              text-white font-semibold
+              py-2.5 px-6
+              rounded-lg
+              mt-6
+              transition duration-200"
+            onClick={handleLogin}
+          >
+            Sign in with Google
+          </button>
+
+        </div>
+      </div>
+    );
+
+};
+
+export default page;
